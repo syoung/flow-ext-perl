6d6705465810738a9fd1ccfb36de862eae130e4e
Stuart Young <stuartpyoung@gmail.com>
2020-06-08 03:13:50 -0400

    -Add bin,html,lib: Installed Net::RabbitFoot, onw add exchange

---
+++ lib/perl5/site_perl/5.26.3/x86_64-linux-thread-multi/auto/share/dist/Alien-Libxml2/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/flow/perl/lib/perl5/site_perl/5.26.3/x86_64-linux-thread-multi/auto/share/dist/Alien-Libxml2/lib"
+XML2_LIBS="-lxml2 -lm "
+XML2_INCLUDEDIR="-I/flow/perl/lib/perl5/site_perl/5.26.3/x86_64-linux-thread-multi/auto/share/dist/Alien-Libxml2/include/libxml2"
+MODULE_VERSION="xml2-2.9.10"
+
